4e55a46279741221b7df787f1a9ea5a517e856ea
Nicolae Dicu <nicolae.dicu.ext@qorix.ai>
2026-07-03 09:53:21 +0300

    Update copyright comments in lic_file.c

--- testing/lic_file.c
+++ testing/lic_file.c
@@ -1,5 +1,5 @@
-# Copyright (c) 2022 Nordic Semiconductor ASA
-#
-# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+// Copyright (c) 2022 Nordic Semiconductor ASA
+//
+// SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
 #include test.h
